867eea9ef590e8c940803af716d8a3a10c0a7ae7
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-01-18 16:25:39 -0300

    created a route of complete and possible habits

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -14,7 +14,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const today = dayjs().startOf("day").toDate();
 
-    prisma.habit.create({
+    await prisma.habit.create({
       data: {
         title,
         created_at: today,
@@ -27,5 +27,47 @@ export async function appRoutes(app: FastifyInstance) {
         },
       },
     });
+  });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
   });
 }

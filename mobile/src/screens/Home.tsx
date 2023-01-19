4f017d23e8556e313c6a1b78bec9ebf3ecd64d79
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-01-19 13:20:15 -0300

    reuse of generate rege in mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,14 @@
-import { View, Text } from "react-native";
-import { DAY_SIZE, HabitDay } from "../components/HabitDay";
+import { View, Text, ScrollView } from "react-native";
+
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
+
 import { Header } from "../components/Header";
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -21,7 +27,24 @@ export function Home() {
         ))}
       </View>
 
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((dates) => (
+            <HabitDay key={`dates-year-${dates}`} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                key={`Day-${i}`}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }

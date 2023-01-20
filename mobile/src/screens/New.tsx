a4564dc1b916d5c0932045c76c4875e371cf9291
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-01-20 12:59:05 -0300

    Created BackButton component

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
+import { View, ScrollView } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <Text>Habit</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+      </ScrollView>
     </View>
   );
 }

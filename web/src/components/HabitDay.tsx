7acfd13619d6dfe1af02f5eaf60cc7aafc656a33
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-01-23 12:54:42 -0300

    Add trasition and focus

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -31,18 +31,21 @@ export function HabitDay({
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10  rounded-lg cursor-pointer", {
-          "bg-zinc-900 border border-zinc-800 ": completedPorcentage == 0,
-          "bg-violet-900 border-violet-700":
-            completedPorcentage > 0 && completedPorcentage < 20,
-          "bg-violet-800 border-violet-600":
-            completedPorcentage >= 20 && completedPorcentage < 40,
-          "bg-violet-700 border-violet-500":
-            completedPorcentage >= 40 && completedPorcentage < 60,
-          "bg-violet-600 border-violet-500":
-            completedPorcentage >= 60 && completedPorcentage < 80,
-          "bg-violet-500 border-violet-400": completedPorcentage >= 80,
-        })}
+        className={clsx(
+          "w-10 h-10  rounded-lg cursor-pointer transition-colors focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-background",
+          {
+            "bg-zinc-900 border border-zinc-800 ": completedPorcentage == 0,
+            "bg-violet-900 border-violet-700":
+              completedPorcentage > 0 && completedPorcentage < 20,
+            "bg-violet-800 border-violet-600":
+              completedPorcentage >= 20 && completedPorcentage < 40,
+            "bg-violet-700 border-violet-500":
+              completedPorcentage >= 40 && completedPorcentage < 60,
+            "bg-violet-600 border-violet-500":
+              completedPorcentage >= 60 && completedPorcentage < 80,
+            "bg-violet-500 border-violet-400": completedPorcentage >= 80,
+          }
+        )}
       />
 
       <Popover.Portal>
